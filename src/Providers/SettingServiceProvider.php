58bdd86a04faabb82814891f5ecf0753b970401d
Hui Yang <chy1988@antwebstudio.com>
2021-05-05 21:12:36 +0800

    Added cache for fusioncms settings, to enhance/optimize performance

--- src/Providers/SettingServiceProvider.php
+++ src/Providers/SettingServiceProvider.php
@@ -7,6 +7,7 @@ use Fusion\Services\Setting as SettingService;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class SettingServiceProvider extends ServiceProvider
@@ -23,7 +24,9 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->settings = SettingService::load();
+        $this->settings = Cache::rememberForever('fusion-settings', function() {
+            return SettingService::load();
+        });
 
         if (settings_available()) {
             $this->bootConfigOverrides();
@@ -55,31 +58,37 @@ class SettingServiceProvider extends ServiceProvider
      */
     private function bootConfigOverrides()
     {
-        SettingGroup::all()->each(function ($group) {
-            if ($group->fieldset) {
-                $group->fieldset->fields->each(function ($field) use ($group) {
-                    if ($field->settings['override'] !== false) {
-                        $key = $field->settings['override'];
-                        $value = setting("{$group->handle}.{$field->handle}");
+        $config = Cache::rememberForever('fusion-setting-config', function() {
+            $config = [];
+            SettingGroup::all()->each(function ($group) use(&$config) {
+                if ($group->fieldset) {
+                    $group->fieldset->fields->each(function ($field) use ($group, &$config) {
+                        if ($field->settings['override'] !== false) {
+                            $key = $field->settings['override'];
+                            $value = setting("{$group->handle}.{$field->handle}");
 
-                        if (is_a($value, Collection::class)) {
-                            $value = $value->all();
+                            if (is_a($value, Collection::class)) {
+                                $value = $value->all();
+                            }
+
+                            // For file types we want to pass the path of the file
+                            // through to override config values.
+                            //
+                            // TODO: Think of a more sustainable way to configure
+                            // these instances so other fields can determine how
+                            // they should override config values.
+                            if ($field->type == 'file' and isset($value[0])) {
+                                $value = storage_path('app/public/'.$value[0]->location);
+                            }
+
+                            $config[$key] = $value;
                         }
-
-                        // For file types we want to pass the path of the file
-                        // through to override config values.
-                        //
-                        // TODO: Think of a more sustainable way to configure
-                        // these instances so other fields can determine how
-                        // they should override config values.
-                        if ($field->type == 'file' and isset($value[0])) {
-                            $value = storage_path('app/public/'.$value[0]->location);
-                        }
-
-                        config([$key => $value]);
-                    }
-                });
-            }
+                    });
+                }
+            });
+            return $config;
         });
+
+        config($config);
     }
 }
