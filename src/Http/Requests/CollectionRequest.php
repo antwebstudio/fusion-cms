9e03e440dba673ecae27391bacf5f45e33222a5f
Hui Yang <huiyang@users.noreply.github.com>
2026-06-13 12:05:23 +0800

    fix some field cannot save when its validation is not set properly

--- src/Http/Requests/CollectionRequest.php
+++ src/Http/Requests/CollectionRequest.php
@@ -84,7 +84,9 @@ class CollectionRequest extends Request
         }
 
         $rules += $this->fields->flatMap(function ($field) {
-            return $field->type()->rules($field, $this->{$field->handle});
+            $rule = $field->type()->rules($field, $this->{$field->handle});
+            
+            return isset($rule) && $rule != [] ? $rule : [$field->handle => 'nullable'];
         })->toArray();
 
         return $rules;
--- src/Http/Requests/TermRequest.php
+++ src/Http/Requests/TermRequest.php
@@ -79,7 +79,9 @@ class TermRequest extends Request
         ];
 
         $rules += $this->fields->flatMap(function ($field) {
-            return $field->type()->rules($field, $this->{$field->handle});
+            $rule = $field->type()->rules($field, $this->{$field->handle});
+            
+            return isset($rule) && $rule != [] ? $rule : [$field->handle => 'nullable'];
         })->toArray();
 
         return $rules;
